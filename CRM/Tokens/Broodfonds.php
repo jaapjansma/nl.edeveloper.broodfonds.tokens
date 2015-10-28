02c261a9184949c125dad2a8bf7246f7786b8b43
Jaap Jansma <jaap@edeveloper.nl>
2015-10-28 17:29:07 +0100

    first commit

---
+++ CRM/Tokens/Broodfonds.php
@@ -0,0 +1,48 @@
+<?php
+
+class CRM_Tokens_Broodfonds {
+
+  protected static $singelton;
+
+  public static function tokens(&$tokens) {
+    $tokens['broodfonds']['broodfonds.geinteresseerden'] = 'Aantal geinteresseerden';
+  }
+
+  public function tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+    if (!empty($tokens['broodfonds'])) {
+      if (in_array('geinteresseerden', $tokens['broodfonds']) || array_key_exists('geinteresseerden', $tokens['broodfonds'])) {
+        $this->geinteresserden($values, $cids, $job, $tokens, $context);
+      }
+    }
+  }
+
+  protected static function geinteresseerden(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+    $geinteresserden = '';
+
+    $cg = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'Broodfonds'));
+    $cf = civicrm_api3('CustomField', 'getsingle', array('custom_group_id' => $cg['id'], 'name' => 'Interesse'));
+    $values = array(
+      1 => array('Ik ga mee doen met het broodfonds', 'String'),
+      2 => array('Ik heb interesse om mee te doen en houd mij op de hoogte', 'String'),
+    );
+
+    $sql = "SELECT COUNT(*) FROM `".$cg['table_name']."` WHERE `".$cf['column_name']."` = %1 OR `".$cf['column_name']."` = %2";
+    $geinteresserden = CRM_Core_DAO::singleValueQuery($sql, $values);
+
+    foreach($cids as $cid) {
+      $values[$cid]['broodfonds.geinteresseerden'] = $geinteresserden;
+    }
+  }
+
+  /**
+   *
+   * @return CRM_Tokens_Broodfonds
+   */
+  public static function singleton() {
+    if (!self::$singelton) {
+      self::$singelton = new CRM_Tokens_Broodfonds();
+    }
+    return self::$singelton;
+  }
+
+}
---
+++ tokens.php
@@ -0,0 +1,175 @@
+<?php
+
+require_once 'tokens.civix.php';
+
+function tokens_civicrm_tokens(&$tokens) {
+  $tokens['tokens']['tokens.today'] = 'Date of Today';
+
+  CRM_Tokens_Broodfonds::tokens($tokens);
+}
+
+function tokens_civicrm_tokenValues(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+  if (!empty($tokens['tokens'])) {
+    if (in_array('today', $tokens['tokens']) || array_key_exists('today', $tokens['tokens'])) {
+      tokens_today($values, $cids, $job, $tokens, $context);
+    }
+  }
+
+  $tokens = CRM_Tokens_Broodfonds::singleton();
+  $tokens->tokenValues($values, $cids, $job, $tokens, $context);
+
+}
+function tokens_today(&$values, $cids, $job = null, $tokens = array(), $context = null) {
+  $months[1] = 'januari';
+  $months[2] = 'februari';
+  $months[3] = 'maart';
+  $months[4] = 'april';
+  $months[5] = 'mei';
+  $months[6] = 'juni';
+  $months[7] = 'juli';
+  $months[8] = 'augustus';
+  $months[9] = 'september';
+  $months[10] = 'oktober';
+  $months[11] = 'november';
+  $months[12] = 'december';
+  $date = new DateTime();
+  foreach($cids as $cid) {
+    $values[$cid]['tokens.today'] = $date->format('d').' '.$months[$date->format('n')].' '.$date->format('Y');
+  }
+}
+
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function tokens_civicrm_config(&$config) {
+  _tokens_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @param $files array(string)
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function tokens_civicrm_xmlMenu(&$files) {
+  _tokens_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function tokens_civicrm_install() {
+  _tokens_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function tokens_civicrm_uninstall() {
+  _tokens_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function tokens_civicrm_enable() {
+  _tokens_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function tokens_civicrm_disable() {
+  _tokens_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @param $op string, the type of operation being performed; 'check' or 'enqueue'
+ * @param $queue CRM_Queue_Queue, (for 'enqueue') the modifiable list of pending up upgrade tasks
+ *
+ * @return mixed
+ *   Based on op. for 'check', returns array(boolean) (TRUE if upgrades are pending)
+ *                for 'enqueue', returns void
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function tokens_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _tokens_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function tokens_civicrm_managed(&$entities) {
+  _tokens_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function tokens_civicrm_caseTypes(&$caseTypes) {
+  _tokens_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function tokens_civicrm_angularModules(&$angularModules) {
+_tokens_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function tokens_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _tokens_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Functions below this ship commented out. Uncomment as required.
+ *
+
+/**
+ * Implements hook_civicrm_preProcess().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
+ *
+function tokens_civicrm_preProcess($formName, &$form) {
+
+}
+
+*/
