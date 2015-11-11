dffeb3039243c37263da9b1daf8c88d5c104df49
Jaap Jansma <jaap@edeveloper.nl>
2015-11-11 12:25:03 +0100

    update token werkend krijgen

--- CRM/Tokens/Broodfonds.php
+++ CRM/Tokens/Broodfonds.php
@@ -12,7 +12,6 @@ class CRM_Tokens_Broodfonds {
     if (!is_array($cids)) {
       $cids = array($cids);
     }
-
     if ($this->checkToken($tokens, 'geinteresseerden')) {
       $this->geinteresseerden($values, $cids, $job, $tokens, $context);
     }
@@ -21,13 +20,13 @@ class CRM_Tokens_Broodfonds {
   protected function geinteresseerden(&$values, $cids, $job = null, $tokens = array(), $context = null) {
     $cg = civicrm_api3('CustomGroup', 'getsingle', array('name' => 'Broodfonds'));
     $cf = civicrm_api3('CustomField', 'getsingle', array('custom_group_id' => $cg['id'], 'name' => 'Interresse'));
-    $values = array(
+    $cf_values = array(
       1 => array('Ik ga mee doen met het broodfonds', 'String'),
       2 => array('Ik heb interesse om mee te doen en houd mij op de hoogte', 'String'),
     );
 
     $sql = "SELECT COUNT(*) FROM `".$cg['table_name']."` WHERE `".$cf['column_name']."` = %1 OR `".$cf['column_name']."` = %2";
-    $geinteresserden = CRM_Core_DAO::singleValueQuery($sql, $values);
+    $geinteresserden = CRM_Core_DAO::singleValueQuery($sql, $cf_values);
 
     foreach($cids as $cid) {
       $this->setValue($values, $cid, 'broodfonds.geinteresseerden', $geinteresserden);
